089b9ff0ee48d56be1e98c451f145df30436bd1e
Alican Akkuş <alican.akkus94@gmail.com>
2014-01-21 18:44:37 +0200

    Update DateExamlpe5.java

--- src/DateExamlpe5.java
+++ src/DateExamlpe5.java
@@ -9,10 +9,10 @@ public class DateExamlpe5 {
 	 */
 	public static void main(final String[] args) {
 		// TODO Auto-generated method stub
-		final Calendar takvim = new GregorianCalendar(2014, 01, 01);// 1 ocak
+		final Calendar takvim = new GregorianCalendar(2014, 01, 01);// 1 ocak tarihini takvim degiskenine atadik
 																	// 2014
 																	// tarihi
-		final Date tarih = takvim.getTime();
+		final Date tarih = takvim.getTime();//tarih nesnesine gün atanabilir..
 		System.out.println(tarih.toString());
 	}
 
